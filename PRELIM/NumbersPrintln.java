be5aa0e33c12147bdcda38e54a5b14d68aeb1dbc
skuuuzk <148032305+skuuuzk@users.noreply.github.com>
2023-10-25 22:31:53 +0800

    NumbersPrintln.java

---
+++ PRELIM/NumbersPrintln.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class NumbersPrintln {
+
+   public static void main(String[] a){
+   
+   int Date = 20;
+   String Name = "Kristipity";
+   
+   System.out.print("You are now ");
+   System.out.print(Date );
+   System.out.print(Name );
+   System.out.print(" yey");
+   }
+
+}
